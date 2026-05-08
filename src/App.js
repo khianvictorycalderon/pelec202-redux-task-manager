439cad75c1677cdac1696bf97059400b82b2bc84
khianvictorycalderon <ckhianvictory@gmail.com>
2026-05-08 13:52:39 +0800

    Completed output section

--- src/App.js
+++ src/App.js
@@ -5,36 +5,37 @@ import { useState } from "react";
 export default function App() {
 
   // Redux
-  const tasks = useSelector(state => state.tasks);
-  const dispath = useDispatch();
-  // ---------------------
+  const tasks = useSelector((state) => state.tasks);
+  const dispatch = useDispatch();
+  // ================================================
 
   const [taskNameInput, setTaskNameInput] = useState("");
 
-  const handleAddTask = async(e) => {
-    e.preventDefault(); // Prevents browser refresh
+  const handleAddTask = (e) => {
+    e.preventDefault();
 
     if (!taskNameInput.trim()) {
       alert("Please enter a task name!");
       return;
     }
 
-    // Generate random ID (No duplicate)
+    // ID generation (prevents duplicates)
     let task_id;
     do {
       task_id = Math.floor(Math.random() * 91049102853);
-    } while (tasks.find(item => item.id === task_id));
+    } while (tasks.find((item) => item.id === task_id));
 
     // Add to redux store
-    dispath(addTask({
-      id: task_id,
-      name: taskNameInput
-    }));
+    dispatch(
+      addTask({
+        id: task_id,
+        name: taskNameInput,
+      })
+    );
 
-    // Clears user input
+    // Clears user input (for ux)
     setTaskNameInput("");
-
-  }
+  };
 
   return (
     <div>
@@ -43,21 +44,33 @@ export default function App() {
       <p className="text-center">by Khian Victory D. Calderon</p>
       <p className="text-center">BSIT - 2A</p>
 
-      <hr/>
+      <hr />
 
-      <form 
-        onSubmit={handleAddTask}
-        className="input-section"
-        >
+      <form onSubmit={handleAddTask} className="input-section">
         <label htmlFor="only-input">Task Name:</label>
-        <input 
-          id="only-input" 
+        <input
+          id="only-input"
           type="text"
-          value={taskNameInput} 
+          value={taskNameInput}
           onChange={(e) => setTaskNameInput(e.target.value)}
         />
         <button type="submit">Add Task</button>
       </form>
+
+      <div className="outputs-wrapper">
+        {tasks.map((task) => (
+          <div 
+            key={task.id}
+            className="output-card"
+            >
+            <span>{task.name}</span>
+            <br/>
+            <button onClick={() => dispatch(deleteTask(task.id))}>
+              Delete
+            </button>
+          </div>
+        ))}
+      </div>
 
     </div>
   );
