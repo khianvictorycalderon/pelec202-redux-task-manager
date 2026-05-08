a1515e9836e0ee5905caf43aee77b76cf1d53029
khianvictorycalderon <ckhianvictory@gmail.com>
2026-05-08 13:22:42 +0800

    Added redux related stuff

---
+++ src/feature/taskSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [];
+
+const taskSlice = createSlice({
+    name: "task",
+    initialState,
+    reducers: {
+        addTask: (state, action) => {
+            state.push(action.payload);
+        },
+        // User may delete the task if it is done already
+        deleteTask: (state, action) => {
+            state.splice(action.payload, 1);
+        }
+    }
+});
+
+export const { addTask, deleteTask } = taskSlice.actions;
+export default taskSlice.reducer;
