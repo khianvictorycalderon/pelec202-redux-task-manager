930d913b2a7bb08966a3bbf3f9cfb88f799d605b
khianvictorycalderon <ckhianvictory@gmail.com>
2026-05-08 13:23:30 +0800

    Imported necessary things

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+import { useDispatch, useSelector } from "react-redux";
+import { addTask, deleteTask } from "./feature/taskSlice";
+import { useState } from "react";
+
 export default function App() {
   return (
     <>
