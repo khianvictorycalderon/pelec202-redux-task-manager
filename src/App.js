00466342b1671509a09ce5199bf1cbe4a88eedfd
khianvictorycalderon <ckhianvictory@gmail.com>
2026-05-08 13:34:06 +0800

    Added input section (UI)

--- src/App.js
+++ src/App.js
@@ -3,8 +3,29 @@ import { addTask, deleteTask } from "./feature/taskSlice";
 import { useState } from "react";
 
 export default function App() {
+
+  const [taskNameInput, setTaskNameInput] = useState("");
+
   return (
-    <>
-    </>
+    <div>
+
+      <p className="text-center">PELEC202 Finals Hands On Quiz 1</p>
+      <p className="text-center">by Khian Victory D. Calderon</p>
+      <p className="text-center">BSIT - 2A</p>
+
+      <hr/>
+
+      <form className="input-section">
+        <label htmlFor="only-input">Task Name:</label>
+        <input 
+          id="only-input" 
+          type="text"
+          value={taskNameInput} 
+          onChange={(e) => setTaskNameInput(e.target.value)}
+        />
+        <button type="submit">Add Task</button>
+      </form>
+
+    </div>
   );
 }
